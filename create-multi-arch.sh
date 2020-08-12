927b2a0f2b17885b7c0a663ace421c578adcc799
Rafael Sene <rpsene@gmail.com>
2020-08-12 15:55:30 -0300

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -2,11 +2,11 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/rpsene/hello-ocp:latest \
+docker manifest create quay.io/rpsene/hello-ocp:v1 \
 quay.io/rpsene/hello-ocp:x86_64 quay.io/rpsene/hello-ocp:ppc64le
 
 docker manifest inspect quay.io/rpsene/hello-ocp
 
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 
-docker manifest push quay.io/rpsene/hello-ocp:latest
+docker manifest push quay.io/rpsene/hello-ocp:v1
