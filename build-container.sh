e3c210366730223ddaf2993563795e104a8210fd
Rafael Sene <rpsene@gmail.com>
2020-08-12 11:08:56 -0300

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -3,6 +3,6 @@
 ARCH=$(uname -m)
 
 docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
-docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
+docker tag container/$ARCH quay.io/rpsene/hello-ocp:$ARCH
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-docker push quay.io/rpsene/multi-arch-travis:$ARCH
+docker push quay.io/rpsene/hello-ocp:$ARCH
