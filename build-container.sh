1c3edeb584b93b1200b61e31e480a9946260ef52
Rafael Sene <rpsene@gmail.com>
2020-08-12 12:40:06 -0300

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -2,6 +2,6 @@
 
 ARCH=$(uname -m)
 
-docker build -t quay.io/rpsene/hello-ocp:$ARCH-f ./Dockerfile.$ARCH .
+docker build -t quay.io/rpsene/hello-ocp:$ARCH -f ./Dockerfile.$ARCH .
 #docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 #docker push quay.io/rpsene/hello-ocp:$ARCH
